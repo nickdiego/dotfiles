4cc2912b8d439bc8e59c0e199641fbce6bcf5a43
Nick Diego Yamane <nick.diego@gmail.com>
2016-09-24 12:44:14 -0400

    Fix more setup.sh issues

--- setup.sh
+++ setup.sh
@@ -1,36 +1,16 @@
 #!/bin/bash
 
-if [ `uname` != 'Linux' ]; then
-  echo '######## Trying to install script in a non-Linux environment!'
-  echo '######## This is still untested and most probably wont work :('
-else
-  sudo pacman -S tmux openssh git nfs-utils highlight \
-    the_silver_searcher acpi \
-    faience-icon-theme gtk-engine-murrine \ # visual stuff
-    graphicsmagick bash-completion autojump xclip
-fi
-
-# download and install manually https://github.com/mclmzz/arc-theme-Red
-
-git submodule update --init
-git submodule foreach git checkout master
-
-pushd .vim/unbundle/general/vim-powerline
-git checkout develop
-popd
-
-BKPDIR="$HOME/.dot-backups"
-[ -d $BKPDIR ] || mkdir -p $BKPDIR
-
 link_dot_file() {
   local file=$1
   local link=$2
-  test -z $link && link=$file
+  local bkpdir="$HOME/.dot-backups"
+  [ -z $link ] && link=$file
+  [ -d $bkpdir ] || mkdir -p $bkpdir
 
-  if [ $PWD/$file == `readlink -f $HOME/$link` ]; then
-    echo "skipping..."
-    return 1
-  elif [ -e $HOME/$file ]; then
+  echo -n "Installing $PWD/$file: "
+  [ $PWD/$file == `readlink -f $HOME/$link` ] &&  { echo "skipping..."; return 1; }
+
+  if [ -e $HOME/$file ]; then
     echo "replacing..."
     mv $HOME/$file $BKPDIR/$file-backup-`date +%Y%m%d%H%M%S`
   else
@@ -40,10 +20,10 @@ link_dot_file() {
 }
 
 link_dot_files() {
-  local ignoredirs="-I '.*~' -I .git -I .gitmodules -I setup.sh -I .kde -I .xdg-config"
-  for file in `ls -A $ignoredirs`;
-  do
-    echo -n "Installing $PWD/$file: "
+  local ignoredirs='-I ".*~" -I .git -I .gitmodules -I setup.sh -I .kde -I .xdg-config'
+  local files_to_install=`eval "ls --color=never -A $ignoredirs"`
+
+  for file in $files_to_install; do
     link_dot_file $file
   done
 }
@@ -57,9 +37,30 @@ install_vim_plugins() {
   vim +PluginInstall +qall
 }
 
+if [ `uname` != 'Linux' ]; then
+  echo '######## Trying to install script in a non-Linux environment!'
+  echo '######## This is still untested and most probably wont work :('
+else
+  sudo pacman -S tmux openssh git nfs-utils highlight \
+    the_silver_searcher acpi \
+    faience-icon-theme gtk-engine-murrine \
+    graphicsmagick bash-completion autojump xclip
+fi
+
+# download and install manually https://github.com/mclmzz/arc-theme-Red
+
+git submodule update --init
+git submodule foreach git checkout master
+
+pushd .vim/unbundle/general/vim-powerline
+git checkout develop
+popd
+
 link_dot_files
+# FIXME generalize this to work with all dirs inside .xdg-config
+link_dot_file .xdg-config/awesome .config/awesome
 install_vim_plugins
-bashrc-reload
+. ~/.bashrc
 
 # TODO instruct to install by the simple way (./install.py --...)
 echo ==========================================================
@@ -73,4 +74,3 @@ echo step 4. Compile the ycm_support_libs libraries
 echo passing the flag -DPATH_TO_LLVM_ROOT=~/bin/clang-binaries
 echo ...
 echo ==========================================================
-
