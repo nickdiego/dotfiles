978b3a79e274693c966262ed6c64d344fc5ea9b2
Nick Diego Yamane <nick.diego@gmail.com>
2016-09-29 17:58:18 -0400

    Add dotfiles project env file

---
+++ .projects-env.d/dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+projname=dotfiles
+
+setenv() {
+  srcdir=${projroot}
+  options=('--sync') # TODO impl support
+}
+
+activate() {
+  # FIXME Temp: Will be moved to 'install'
+  # function latter.
+  if (( ${_opt[--sync]} )); then
+    echo "Let's sync the dotfiles :)"
+    cd_project $projname
+    ./sync.sh
+  fi
+}
+
