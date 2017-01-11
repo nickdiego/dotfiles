48605ddbdc73ae26a886fc8fe2041fa9c816986a
Nick Diego Yamane <nick.diego@gmail.com>
2017-01-11 10:34:09 -0400

    toolsenv: Improve qt config script

--- .tools-env.d/qt.sh
+++ .tools-env.d/qt.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
-custom_tool_config() {
-  local tool=$1
-  test -d $tool || return 1
-  [ ! -z $verbose_env ] && echo Setting env for $tool
+qt_config() {
+  local qt_rootdir=${QT_ROOTDIR_DEFAULT:=$tools_dir/qt-5}
+  local version=${QT_VERSION_DEFAULT:=5.7}
+  local target_plat=${QT_TARGETPLAT_DEFAULT:='android_armv7'}
+  while (( $# )); do
+    case $1 in
+      --version)
+        shift && version=$1;;
+      --target)
+        shift && target_plat=$1;;
+      # TODO impl root dir
+    esac
+    shift
+  done
 
-  export QT_ROOT=$tool
-  export ANDROID_NATIVE_API_LEVEL=android-9
-  export ANT_LOCATION=/usr/bin/ant
-  export PATH="$tools_dir/qt-5/Tools/QtCreator/bin:$PATH"
+  local qt_toolsdir="${qt_rootdir}/Tools"
+  if [[ -d ${qt_toolsdir}/QtCreator ]]; then
+    export PATH="${qt_toolsdir}/QtCreator/bin:$PATH"
+  else
+    (( $verbose_env )) && echo "QtCreator not found!"
+  fi
+
+  local qt_versiondir="${qt_rootdir}/${version}"
+  test -d $qt_versiondir || return 1
+
+  (( $verbose_env )) && echo Setting env for $qt_rootdir
+  export QTDIR=$qt_versiondir
+  # TODO set QMAKESPEC ?
+
+  local qt_targetplatdir="${qt_versiondir}/${target_plat}"
+  test -d $qt_targetplatdir || {
+    echo "Failed config Qt: invalid target platform dir $qt_targetplatdir"
+    unset -v QT_PLATFORM_DIR
+    return 1
+  }
+
+  export QT_PLATFORM_DIR="${qt_versiondir}/${target_plat}"
+  export QT_PLATFORMS=( $(basename -a ${qt_versiondir}/*) )
+
+  if [[ ${QT_PLATFORMS[@]} =~ android_armv7 ]]; then
+    export ANDROID_NATIVE_API_LEVEL=android-9
+    export ANT_LOCATION=/usr/bin/ant
+  fi
 }
 
-custom_tool_config "$tools_dir/qt-5/5.7/android_armv7"
-#custom_tool_config '/usr/local/Qt-5.7.0/'
-#custom_tool_config '/usr/local/Qt-5.7.0-Android-dhg'
-unset custom_tool_config
+qt_config
 
+# TODO implement bash completion support
