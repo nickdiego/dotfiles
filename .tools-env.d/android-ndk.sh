473f77e68f63df9bf52044760bbace9e7134ece7
Nick Diego Yamane <nick.diego@gmail.com>
2017-02-03 03:44:49 -0400

    tools-env: Add android-ndk tools to the PATH

--- .tools-env.d/android-ndk.sh
+++ .tools-env.d/android-ndk.sh
@@ -10,7 +10,8 @@ custom_tool_config() {
   export ANDROID_NDK=$tool
   export ANDROID_NDK_TOOLCHAIN_ROOT="${ANDROID_NDK}/toolchains"
   export ANDROID_NDK_r12b=$tool
-  export PATH="$PATH:$ANDROID_NDK"
+  export ANDROID_NDK_TOOLCHAIN_ARM="${ANDROID_NDK_TOOLCHAIN_ROOT}/arm-linux-androideabi-4.9/prebuilt/linux-x86_64"
+  export PATH="$PATH:${ANDROID_NDK_TOOLCHAIN_ARM}/bin"
 }
 
 custom_tool_config 'android-ndk'
