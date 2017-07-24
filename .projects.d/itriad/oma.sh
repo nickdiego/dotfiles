7665d8b2efc8ba754ddcd1feb4d77c42b08b2eba
Nick Diego Yamane <nick.diego@gmail.com>
2017-07-23 21:31:56 -0400

    proj-env: add aosp docker subproj option
    Signed-off-by: Nick Diego Yamane <nick.diego@gmail.com>

--- .projects.d/itriad/oma.sh
+++ .projects.d/itriad/oma.sh
@@ -12,6 +12,7 @@ setenv() {
       ;;
     aosp)
       dirs[src]="src/aosp-src/aosp"
+      options=(--docker)
       ;;
   esac
 }
@@ -19,6 +20,10 @@ setenv() {
 activate() {
   #dirs[build]="build/$target"
   target=${targets[0]} # FIXME get from parameters
+
+  if [ "$subproj" = 'aosp' ] && (( ${_opt[--docker]} )); then
+    ( cd "${dirs[root]}/${dirs[src]}/.." && aosp; )
+  fi
 
   # Call env initialization script if exists
   [ -r ${dirs[root]}/env.sh ] && . ${dirs[root]}/env.sh $target
