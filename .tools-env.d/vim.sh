e72539d601a382a21f056683ecefee2ea1c76659
Nick Diego Yamane <nick@diegoyam.com>
2018-04-08 03:38:09 -0400

    tools-env: extract vim shell helpers into its own env script
    Signed-off-by: Nick Diego Yamane <nick@diegoyam.com>

---
+++ .tools-env.d/vim.sh
@@ -0,0 +1,27 @@
+# ex: ts=4 sw=4 et filetype=sh
+
+VIM_SESSIONS_DIR="${HOME}/.vim/sessions"
+declare -a VIM_SESSIONS
+
+# Initialize VIM_SESSIONS var
+if [ -d "$VIM_SESSIONS_DIR" ]; then
+    VIM_SESSIONS=( $(cd $VIM_SESSIONS_DIR; echo *) )
+fi
+
+# Open vim session
+v() {
+  local session=${1:-${curr_proj_vimsession}}
+
+  if [[ -z $session ]]; then
+    echo "No session passed as argument nor \$curr_proj_vimsession set!" >&2
+    return 1
+  fi
+
+  eval "vim '+SessionOpen $session' '+set columns=$COLUMNS'"
+}
+
+if type complete &>/dev/null; then
+    complete -W "${VIM_SESSIONS[*]}" v
+elif type compctl &>/dev/null; then
+    compctl -k "(${VIM_SESSIONS[*]})" v
+fi
