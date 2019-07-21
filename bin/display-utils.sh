f7eabebe5fb49107a391dc0e4845d0a2c2ff793f
Nick Diego Yamane <nickdiego@igalia.com>
2019-07-21 00:02:59 -0400

    update display helper functions and start using autorandr
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- bin/display-utils.sh
+++ bin/display-utils.sh
@@ -11,22 +11,37 @@ else # assuming Running Xorg
   case `hostname` in
     mayam)
         export DISPLAY_MAIN='eDP1'
-        export DISPLAY_AUX='DP1'
-        ;;
-    *)
-        export DISPLAY_MAIN='LVDS1'
-        export DISPLAY_AUX='HDMI1'
+        export DISPLAY_AUX=('DP1' 'DP2')
+        export DISPLAY_CMD_ENABLE_AUX="\
+          --output eDP1 --off \
+          --output DP2 --auto --primary \
+          --output DP1 --auto --left-of DP2"
         ;;
   esac
 fi
 
 # Add new xrandr display mode
-add-display-mode() {
+display_add_mode() {
   local hres=${1:-1440} vres=${2:-900}
   local name=${3:-"${hres}x${vres}_custom"}
   local mode=$(cvt $hres $vres | tail -n1 | sed 's,^Modeline ".\+"\s\+,,')
   xrandr --newmode "$name" $mode 2>/dev/null || return 1
 }
 
-_arrange_displays_opts=( --disable --enable )
+display_disable_aux() {
+  declare -a args
+  for display in "${DISPLAY_AUX[@]}"; do
+    args+=( --output $display --off )
+  done
+  xrandr "${args[@]}" --output $DISPLAY_MAIN --auto
+}
 
+display_enable_aux() {
+  if [ -z "$DISPLAY_CMD_ENABLE_AUX" ]; then
+    # TODO: Generate default cmd
+    echo "Display config not set for this machine." >&2
+    return 1
+  fi
+
+  xrandr $DISPLAY_CMD_ENABLE_AUX
+}
