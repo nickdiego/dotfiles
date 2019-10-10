7c0406915c8f4a0fcd3cd2e64bef792fa3608198
Nick Diego Yamane <nickdiego@igalia.com>
2019-10-09 23:26:01 -0400

    develenv: Add {x,wl}session helpers
    They are meant respectively to help on starting X11 and Wayland
    sessions. Both support bash and zsh command line completion.
    
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

---
+++ bin/graphical-session-helpers.sh
@@ -0,0 +1,28 @@
+# vim: tw=80 ts=2 sw=2 et filetype=sh
+
+# xsession
+_xsession_opts=(i3 gnome kde xfce)
+function xsession() {
+  startx ~/.xinitrc $@
+}
+
+# wlsession
+_wlsession_opts=(gnome sway weston)
+function wlsession() {
+  local s=${1:-gnome}
+  [ $s = 'gnome' ] && s+='-session'
+  env QT_QPA_PLATFORM=wayland \
+    GDK_BACKEND=wayland,x11 \
+    XDG_SESSION_TYPE=wayland \
+      dbus-run-session $s
+}
+
+# bash/zsh completion
+if type complete &>/dev/null; then
+    complete -W "${_xsession_opts[*]}" xsession
+    complete -W "${_wlsession_opts[*]}" wlsession
+elif type compctl &>/dev/null; then
+    compctl -k "(${_xsession_opts[*]})" xsession
+    compctl -k "(${_wlsession_opts[*]})" wlsession
+fi
+
