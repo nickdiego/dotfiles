99f54b9cf8831bbe56e608b49c0582f481fd9454
Nick Diego Yamane <nickdiego@igalia.com>
2020-04-11 15:43:28 -0400

    wayland: Add wl_record_screen helper function
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- bin/graphical-session-helpers.sh
+++ bin/graphical-session-helpers.sh
@@ -20,6 +20,50 @@ function wlsession() {
       dbus-run-session $s
 }
 
+function wl_record_screen() {
+  type swaymsg &>/dev/null || { echo "swaymsg not installed!"; return 1; }
+  type wf-recorder &>/dev/null || { echo "wf-recorder not installed!"; return 1; }
+  test -n "$WAYLAND_DISPLAY" || { echo "WAYLAND_DISPLAY not set!"; return 1; }
+  type ffplay &>/dev/null || { echo "ffplay not installed!"; return 1; }
+
+  local -a opts
+  local outfile='screencast.mp4'
+  local reclog='/tmp/scrrec.log'
+  local play=0
+
+  while (( $# )); do
+    case $1 in
+      -p|--play)
+        play=1
+        ;;
+      -*|--*)
+        opts+=("$1")
+        ;;
+      *)
+        outfile=$1
+        ;;
+    esac
+    shift
+  done
+
+  local jq_query='.[] | select(.active) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'
+  if false; then # TODO: Support selecting a screen area?
+    local rect=$(swaymsg -t get_outputs | jq -r "$jq_query" | slurp)
+    opts+=(-g "$rect")
+    echo "Capturing rect ${rect}..."
+  fi
+
+  echo "Recording... outfile=${outfile}"
+  wf-recorder "${opts[@]}" -f "$outfile" 2>$reclog
+  if [ $? -ne 0 ]; then
+    echo "Failed to record screen!!";
+    cat -n $reclog
+    return 1;
+  fi
+
+  (( play )) && echo "Playing..." && ffplay $outfile
+}
+
 # bash/zsh completion
 if type complete &>/dev/null; then
     complete -W "${_xsession_opts[*]}" xsession
