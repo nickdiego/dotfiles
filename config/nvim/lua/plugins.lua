10d2c1428484bb58ebee5ccfd9bc7f356a339992
Nick Diego Yamane <nickdiego@igalia.com>
2025-04-24 16:20:21 -0400

    nvim: remove nvim-treesitter for now (unused)
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -56,27 +56,6 @@ return {
   { 'rhysd/committia.vim' },
   { 'chromium/vim-codesearch' },
   { 'neovim/nvim-lspconfig' },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { "lua", "vim", "python", "c", "cpp", "json", "html", "css" },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true
-        },
-        fold = {
-          enable = true,
-        },
-      }
-    end
-  },
 
   -- completion
   { 'hrsh7th/nvim-cmp' },
