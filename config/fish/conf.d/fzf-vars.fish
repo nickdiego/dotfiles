103c483b89f843c51d4539ae03867ef32e25e93b
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-08 04:02:24 -0400

    fish: fix ctrl+p fzf-marks keybinding
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- config/fish/conf.d/fzf-vars.fish
+++ config/fish/conf.d/fzf-vars.fish
@@ -1,3 +1,7 @@
+set -gx FZF_MARKS_JUMP \cp
+bind -M insert $FZF_MARKS_JUMP fzm
+bind $FZF_MARKS_JUMP fzm
+
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --exclude .git'
 set -gx FZF_DEFAULT_OPTS '--reverse'
 set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
