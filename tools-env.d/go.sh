f0270c5e810cd84887e7824fdea341a43c6da960
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-08 00:41:34 -0400

    env: clean up old tools env helper scripts
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- tools-env.d/go.sh
+++ tools-env.d/go.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+# vim: ts=4 sw=4 et filetype=sh
 
-export PATH="$HOME/go/bin:$PATH"
+test -d "${HOME}/go/bin" && export PATH="${HOME}/go/bin:$PATH"
--- tools-env.d/rust.sh
+++ tools-env.d/rust.sh
@@ -1,3 +1,3 @@
 # vim: ts=4 sw=4 et filetype=sh
 
-export PATH="${PATH}:${HOME}/.cargo/bin"
+test -d "${HOME}/.cargo" && export PATH="${PATH}:${HOME}/.cargo/bin"
