e931c90b0abd2b8f27484b37f67ced1c83a3a8b1
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-08 05:49:45 -0400

    fish: ensure  BASE16_THEME env var gets set when theme is set
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- config/fish/conf.d/base16.fish
+++ config/fish/conf.d/base16.fish
@@ -4,3 +4,10 @@ if test -e ~/.config/base16-shell/profile_helper.fish
         base16-default-dark
     end
 end
+
+# profile_helper.fish has a bug: it uses $BASE16_THEME (unset) instead of
+# $SCRIPT_NAME when deriving the theme name, and the .sh scripts run in a
+# subshell so their `export BASE16_THEME=...` never reaches fish. Set it here.
+if test -e ~/.base16_theme; and test -z "$BASE16_THEME"
+    set -gx BASE16_THEME (basename (realpath ~/.base16_theme) .sh | string replace 'base16-' '')
+end
