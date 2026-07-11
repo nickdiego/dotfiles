4f2fb1f915cd33c1a8a386795a8dace2d13510db
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-11 16:44:23 -0400

    nvim: fix lua_ls config: add root_markers, LuaJIT runtime, disable checkThirdParty

--- config/nvim/lua/config/lsp.lua
+++ config/nvim/lua/config/lsp.lua
@@ -67,11 +67,14 @@ vim.lsp.config('clangd', {
 })
 
 vim.lsp.config('lua_ls', {
+  root_markers = { '.luarc.json', '.luarc.jsonc', '.stylua.toml', 'stylua.toml', '.git' },
   settings = {
     Lua = {
+      runtime = { version = 'LuaJIT' },
       diagnostics = { globals = { 'vim' } },
       workspace = {
         library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
       },
       telemetry = { enable = false },
     },
