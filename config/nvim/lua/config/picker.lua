2e1c3bdd1b86553c2332ada1ecadf9d452e737ec
Nick Diego Yamane <nickdiego@igalia.com>
2026-07-15 01:31:33 -0400

    nvim: update snacks picker keybindings
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- config/nvim/lua/config/picker.lua
+++ config/nvim/lua/config/picker.lua
@@ -2,7 +2,9 @@
 
 vim.keymap.set({ 'n', 'i' }, '<C-p>', function() Snacks.picker.smart() end,
   { noremap = true, silent = true, desc = 'Open Files (snacks)' })
-vim.keymap.set({ 'n', 'i' }, '<C-A-f>', function() Snacks.picker.grep() end,
+vim.keymap.set({ 'n', 'i' }, '<C-f>', function() Snacks.picker.grep() end,
   { noremap = true, silent = true, desc = 'Live grep (snacks)' })
 vim.keymap.set({ 'n', 'i' }, '<C-A-k>', function() Snacks.picker.keymaps() end,
   { noremap = true, silent = true, desc = 'List keybindings (snacks)' })
+vim.keymap.set({ 'n' }, '<Leader>r', function() Snacks.picker.resume() end,
+  { noremap = true, silent = true, desc = 'Resume picker (snacks)' })
